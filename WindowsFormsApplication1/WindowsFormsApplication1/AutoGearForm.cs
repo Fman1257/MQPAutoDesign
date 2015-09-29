a574e7b13073311007b186bf62bbe8400cf2c94a
dgrande77 <dgrande770@gmail.com>
2015-09-28 21:32:10 -0400

    Commit Test Again.

--- WindowsFormsApplication1/WindowsFormsApplication1/AutoGearForm.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/AutoGearForm.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void DesignButton_Click(object sender, EventArgs e)
         {
-            /*
+            
             float length;
             float width;
             float height;
@@ -38,7 +38,7 @@ namespace WindowsFormsApplication1
             {
 
             }
-            */
+            
         }
     }
 }
